df39ff889773436111e41df5407a7989a858e1a4
fkrfdllh <fkrfdllh@gmail.com>
2016-10-13 07:56:27 +0700

    zz

---
+++ resources/views/about.blade.php
@@ -0,0 +1,42 @@
+
+@extends('app')
+
+@section('header')
+
+    <title>Laravel &raquo; About</title>
+
+@endsection
+
+@section('content')
+
+
+	<br>
+	<br>
+	<br>
+	<br>
+	<br>
+	<center>
+    <h1></h1>
+	<h1>Hello World</h1>
+	<h3>LARAVEL 5.3</h3>
+	<br>
+	<br>
+	<br>
+	<br>
+		<br>
+	<br>
+	<br>
+	<br>
+		<br>
+		<br>
+	</center>
+	<footer>
+	<center>
+	<h6>Created By Adrian</h6>
+	<h6>Contact Us</h6>
+	<h6>Email : arm.adrian02@gmail.com</h6>
+	</center>
+	</footer>
+
+
+@endsection
